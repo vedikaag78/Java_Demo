5d879ed4fbfa78ce0b8f4653e0e1a288baa50967
Vedika Agrawal <vedika.agrawal@fk1089-215056l.fkipl.flipkart.com>
2024-06-25 15:28:42 +0530

    first commit

---
+++ src/com/flipkart/test/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.test;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first java program"); //SHORTCUT - syso
+		
+
+	}
+
+}
